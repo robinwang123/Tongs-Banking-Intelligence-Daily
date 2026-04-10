af3af960172cc992f31eb118d889f5045928e341
Tong <43728173+robinwang123@users.noreply.github.com>
2026-04-10 13:42:29 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -4,7 +4,7 @@
 
 import { buildEmailHtml, buildSubject } from './_email.js';
 
-const FROM = 'Tongs Banking Intelligence Daily <onboarding@resend.dev>';
+const FROM = 'Tongs Banking Intelligence Daily <TongsKIDaily@amongthelight.com>';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
